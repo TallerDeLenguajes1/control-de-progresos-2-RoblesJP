df4494059c1cce8def6eba8ca8f2cf3be9e398c0
RoblesJP <juan.robles1417@gmail.com>
2020-06-02 16:02:03 -0300

    Creada class Personaje

---
+++ JuegoDeRol/Personaje/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Personaje
+{
+    class Personaje
+    {
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
+        private int armadura;
+        private int salud;
+        private string tipo;
+        private string nombre;
+        private string apodo;
+        private DateTime nacimiento;
+        int Edad(DateTime nacimiento)
+        {
+            int edad = 0;
+            return edad;
+        }
+    }
+}
