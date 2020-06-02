f46f64dec4bf7fe8a4b57aead550a686e77c62d9
RoblesJP <juan.robles1417@gmail.com>
2020-06-02 16:08:08 -0300

    Fix

--- JuegoDeRol/Personaje/Program.cs
+++ JuegoDeRol/Personaje/Program.cs
@@ -4,16 +4,16 @@ namespace Personaje
 {
     class Personaje
     {
-        private int velocidad;
-        private int destreza;
-        private int fuerza;
-        private int nivel;
-        private int armadura;
-        private int salud;
-        private string tipo;
-        private string nombre;
-        private string apodo;
-        private DateTime nacimiento;
+        int velocidad;
+        int destreza;
+        int fuerza;
+        int nivel;
+        int armadura;
+        int salud;
+        string tipo;
+        string nombre;
+        string apodo;
+        DateTime nacimiento;
         int Edad(DateTime nacimiento)
         {
             int edad = 0;
