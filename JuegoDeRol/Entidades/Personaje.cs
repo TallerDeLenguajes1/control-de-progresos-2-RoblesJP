a4f0ef575dc7d1c3e2851dc2c1d3e897aa85d256
RoblesJP <juan.robles1417@gmail.com>
2020-06-03 22:18:16 -0300

    Las caracteristicas y los datos de un personaje pasan a ser class seperadas

--- JuegoDeRol/Entidades/Personaje.cs
+++ JuegoDeRol/Entidades/Personaje.cs
@@ -4,42 +4,12 @@ using System.Text;
 
 namespace Entidades
 {
-    public enum Tipos
-    {
-        Tipo1,
-        Tipo2,
-        Tipo3,
-        Tipo4
-    }
-
     public class Personaje
     {
-        int velocidad;
-        int destreza;
-        int fuerza;
-        int nivel;
-        int armadura;
-        int salud;
-        Tipos tipo;
-        string nombre;
-        string apodo;
-        DateTime nacimiento;
+        private Caracteristicas caracteristicas = new Caracteristicas();
+        private Datos datos = new Datos();
 
-        public int Velocidad { get => velocidad; set => velocidad = value; }
-        public int Destreza { get => destreza; set => destreza = value; }
-        public int Fuerza { get => fuerza; set => fuerza = value; }
-        public int Nivel { get => nivel; set => nivel = value; }
-        public int Armadura { get => armadura; set => armadura = value; }
-        public int Salud { get => salud; set => salud = value; }
-        public Tipos Tipo { get => tipo; set => tipo = value; }
-        public string Nombre { get => nombre; set => nombre = value; }
-        public string Apodo { get => apodo; set => apodo = value; }
-        public DateTime Nacimiento { get => nacimiento; set => nacimiento = value; }
-
-        int Edad(DateTime nacimiento)
-        {
-            int edad = 0;
-            return edad;
-        }
+        public Caracteristicas Caracteristicas { get => caracteristicas; set => caracteristicas = value; }
+        public Datos Datos { get => datos; set => datos = value; }
     }
 }
