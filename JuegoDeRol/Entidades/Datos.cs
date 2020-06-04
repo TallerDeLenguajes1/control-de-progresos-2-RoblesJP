b6767b53b0d8dff6fc9dd847e6c71d7391d494e3
RoblesJP <juan.robles1417@gmail.com>
2020-06-03 22:11:59 -0300

    Creada class Datos

---
+++ JuegoDeRol/Entidades/Datos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Datos
+    {
+        private int salud;
+        private string tipo;
+        private string nombre;
+        private string apodo;
+        private DateTime nacimiento;
+
+        public int Salud { get => salud; set => salud = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public DateTime Nacimiento { get => nacimiento; set => nacimiento = value; }
+
+        public int Edad(DateTime fecha)
+        {
+            int edad = fecha.Year - Nacimiento.Year;
+            return edad;
+        }
+    }
+}
