86db8b609142e17db259e0d5f073d64ce7024f77
RoblesJP <juan.robles1417@gmail.com>
2020-06-04 22:04:48 -0300

    Creado constructor

--- JuegoDeRol/Entidades/Datos.cs
+++ JuegoDeRol/Entidades/Datos.cs
@@ -6,17 +6,26 @@ namespace Entidades
 {
     public class Datos
     {
-        private int salud;
-        private string tipo;
         private string nombre;
         private string apodo;
+        private string tipo;
         private DateTime nacimiento;
+        private int salud;
 
-        public int Salud { get => salud; set => salud = value; }
-        public string Tipo { get => tipo; set => tipo = value; }
         public string Nombre { get => nombre; set => nombre = value; }
         public string Apodo { get => apodo; set => apodo = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
         public DateTime Nacimiento { get => nacimiento; set => nacimiento = value; }
+        public int Salud { get => salud; set => salud = value; }
+
+        public Datos(string nombre, string apodo, string tipo, DateTime nacimiento, int salud)
+        {
+            Nombre = nombre;
+            Apodo = apodo;
+            Tipo = tipo;
+            Nacimiento = nacimiento;
+            Salud = salud;
+        }
 
         public int Edad(DateTime fecha)
         {
