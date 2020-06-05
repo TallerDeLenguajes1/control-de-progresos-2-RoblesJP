794550d393ed6da4cce66916b028b312e58a5d56
RoblesJP <juan.robles1417@gmail.com>
2020-06-04 22:04:20 -0300

    Creado constructor

--- JuegoDeRol/Entidades/Caracteristicas.cs
+++ JuegoDeRol/Entidades/Caracteristicas.cs
@@ -6,16 +6,25 @@ namespace Entidades
 {
     public class Caracteristicas
     {
+        private int nivel;
+        private int fuerza;
         private int velocidad;
         private int destreza;
-        private int fuerza;
-        private int nivel;
         private int armadura;
 
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
         public int Velocidad { get => velocidad; set => velocidad = value; }
         public int Destreza { get => destreza; set => destreza = value; }
-        public int Fuerza { get => fuerza; set => fuerza = value; }
-        public int Nivel { get => nivel; set => nivel = value; }
         public int Armadura { get => armadura; set => armadura = value; }
+
+        public Caracteristicas(int nivel, int fuerza, int velocidad, int destreza, int armadura)
+        {
+            Nivel = nivel;
+            Fuerza = fuerza;
+            Velocidad = velocidad;
+            Destreza = destreza;
+            Armadura = armadura;
+        }
     }
 }
