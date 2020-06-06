b7f5e6a3cd500af5907bea0e252d2c4451a1a276
RoblesJP <juan.robles1417@gmail.com>
2020-06-05 21:37:13 -0300

    Creado enum Max

--- JuegoDeRol/Entidades/Personaje.cs
+++ JuegoDeRol/Entidades/Personaje.cs
@@ -4,6 +4,42 @@ using System.Text;
 
 namespace Entidades
 {
+    public enum Nombres
+    {
+        Jorge = 0,
+        Miguel = 1,
+        Pedro = 2,
+        Vanessa = 3,
+        Mai = 4,
+    }
+
+    public enum Apodos // https://www.fantasynamegenerators.com/warrior-nicknames.php
+    {
+        Shieldmight = 0,
+        Boulderpelt = 1,
+        Strongblow = 2,
+        Ravenshield = 3,
+        Sharpvisage = 4,
+    }
+
+    public enum Tipos
+    {
+        Gerrero = 0,
+        Mago = 1,
+        Monje = 2,
+        Ladron = 3,
+        Arquero = 4,
+    }
+
+    public enum Max
+    {
+        Nivel = 10,
+        Fuerza = 10,
+        Velocidad = 10,
+        Destreza = 5,
+        Armadura = 10,
+    }
+
     public class Personaje
     {
         private Datos datos;
