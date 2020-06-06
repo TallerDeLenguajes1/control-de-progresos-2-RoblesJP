70dd2a533111017eaf16a51e7374c5b0cda8113f
RoblesJP <juan.robles1417@gmail.com>
2020-06-05 21:36:38 -0300

    Enums movidos a la class Personaje

--- JuegoDeRol/Main/Program.cs
+++ JuegoDeRol/Main/Program.cs
@@ -6,40 +6,13 @@ namespace Main
 {
     class Program
     {
-        enum Nombres
-        {
-            Jorge = 0,
-            Miguel = 1,
-            Pedro = 2,
-            Vanessa = 3,
-            Mai = 4,
-        }
-
-        enum Apodos // https://www.fantasynamegenerators.com/warrior-nicknames.php
-        {
-            Shieldmight,
-            Boulderpelt,
-            Strongblow,
-            Ravenshield,
-            Sharpvisage,
-        }
-
-        public enum Tipos
-        {
-            Gerrero,
-            Mago,
-            Monje,
-            Ladron,
-            Arquero,
-        }
-
         static void Main(string[] args)
         {
             Random rnd = new Random();
             Caracteristicas caracteristicas;
             Datos datos;
             Personaje personaje;
-            List<Personaje> personajes = new List<Personaje>();
+            List<Personaje> personajes = new List<Personaje>(); // lista que contiene a todos los personajes
             int indiceNombre; // indice para obtener valor aleatorio del enum
             int indiceApodo;
             int indiceTipos;
@@ -49,26 +22,26 @@ namespace Main
             for (int i = 0; i < cantPersonajes; i++)
             {
                 // datos
-                indiceNombre = rnd.Next(Enum.GetNames(typeof(Nombres)).Length);
-                indiceApodo = rnd.Next(Enum.GetNames(typeof(Apodos)).Length);
+                indiceNombre = i;
+                indiceApodo = i;
                 indiceTipos = rnd.Next(Enum.GetNames(typeof(Tipos)).Length);
                 DateTime comienzo = new DateTime(1200, 1, 1);
                 DateTime final = new DateTime(1500, 12, 31);
                 datos = new Datos(
-                                    Convert.ToString((Nombres)indiceNombre),    // nombre
-                                    Convert.ToString((Apodos)indiceApodo),      // apodo
-                                    Convert.ToString((Tipos)indiceTipos),       // tipo
-                                    FechaAleatoria(comienzo, final),            // nacimiento (todos nacen a las 12AM)
-                                    rnd.Next(101)                               // salud
+                                    Convert.ToString((Nombres)indiceNombre),            // nombre
+                                    Convert.ToString((Apodos)indiceApodo),              // apodo
+                                    Convert.ToString((Tipos)indiceTipos),               // tipo
+                                    FechaAleatoria(comienzo, final),                    // nacimiento (todos nacen a las 12AM)
+                                    rnd.Next(101)                                       // salud
                     );
 
                 // caracteristicas
                 caracteristicas = new Caracteristicas(
-                                    rnd.Next(10) + 1,                           // nivel
-                                    rnd.Next(10) + 1,                           // fuerza
-                                    rnd.Next(10) + 1,                           // velocidad
-                                    rnd.Next(5) + 1,                            // destreza
-                                    rnd.Next(10) + 1                            // armadura
+                                    rnd.Next(Convert.ToInt32(Max.Nivel)) + 1,           // nivel
+                                    rnd.Next(Convert.ToInt32(Max.Fuerza)) + 1,          // fuerza
+                                    rnd.Next(Convert.ToInt32(Max.Velocidad)) + 1,       // velocidad
+                                    rnd.Next(Convert.ToInt32(Max.Destreza)) + 1,        // destreza
+                                    rnd.Next(Convert.ToInt32(Max.Armadura)) + 1         // armadura
                     );
 
                 personaje = new Personaje(datos, caracteristicas);
