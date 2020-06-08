2f324b7f4264439ec3b5ca4ffead49be527295a9
RoblesJP <juan.robles1417@gmail.com>
2020-06-08 15:57:30 -0300

    Eliminada funcion Batalla(), proceso movido a Main; modificada funcion Combate()

--- JuegoDeRol/Main/Program.cs
+++ JuegoDeRol/Main/Program.cs
@@ -11,12 +11,29 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
             int cantPersonajes = 5;
-            List<Personaje> personajes = cargarPersonajes(cantPersonajes); // lista que contiene a todos los personajes
-            mostrarPersonajes(personajes);
-            Personaje reyDeLosSieteReinos = Batalla(personajes);
-            Console.WriteLine("\nEl Rey de los Siete Reinos es {0} \'{1}\'", reyDeLosSieteReinos.Datos.Nombre, reyDeLosSieteReinos.Datos.Apodo);
+            List<Personaje> personajes = CargarPersonajes(cantPersonajes); // lista que contiene a todos los personajes
+            Personaje[] resultadoCombate;
+            int nroCombate = 0;
+            Personaje reyDeLosSieteReinos;
+
+            MostrarPersonajes(personajes);
+
+            // batalla
+            while (personajes.Count > 1)
+            {
+                nroCombate++;
+                Console.WriteLine("\n--- Combate #{0} ({1} vs {2}) ---",    nroCombate,
+                                                                            personajes[0].Datos.Nombre + " " + personajes[0].Datos.Apodo, 
+                                                                            personajes[1].Datos.Nombre + " " + personajes[1].Datos.Apodo);
+                resultadoCombate = Combate(personajes[0], personajes[1]);
+                Console.WriteLine("Ganador del combate: {0} con {1} de salud",  resultadoCombate[0].Datos.Nombre + " " + resultadoCombate[0].Datos.Apodo,
+                                                                                resultadoCombate[0].Datos.Salud);
+                personajes.Remove(resultadoCombate[1]);
+            }
+            reyDeLosSieteReinos = personajes[0];
+            Console.WriteLine("\n--- Batalla finalizada---");
+            Console.WriteLine("El nuevo Rey de los Siete Reinos es: {0}", reyDeLosSieteReinos.Datos.Nombre + " " + reyDeLosSieteReinos.Datos.Apodo);
         }
 
         static DateTime FechaAleatoria(DateTime comienzo, DateTime final)
@@ -26,7 +43,7 @@ namespace Main
             return comienzo.AddDays(rnd.Next(rango));
         }
 
-        static List<Personaje> cargarPersonajes(int cant)
+        static List<Personaje> CargarPersonajes(int cant)
         {
             List<Personaje> personajes = new List<Personaje>();
             Random rnd = new Random();
@@ -54,21 +71,21 @@ namespace Main
                 } while ((final.Year - fechaNacimieno.Year) < 50);  // Los personajes tendran entre 50 y 300 años
 
                 datos = new Datos(
-                                    Convert.ToString((Nombres)indiceNombre),            // nombre
-                                    Convert.ToString((Apodos)indiceApodo),              // apodo
-                                    Convert.ToString((Tipos)indiceTipos),               // tipo
-                                    fechaNacimieno,                                     // nacimiento (todos nacen a la misma hora)
-                                    rnd.Next(101)                                       // salud
-                    );
+                    Convert.ToString((Nombres)indiceNombre),            // nombre
+                    Convert.ToString((Apodos)indiceApodo),              // apodo
+                    Convert.ToString((Tipos)indiceTipos),               // tipo
+                    fechaNacimieno,                                     // nacimiento (todos nacen a la misma hora)
+                    rnd.Next(101)                                       // salud
+                );
 
                 // caracteristicas
                 caracteristicas = new Caracteristicas(
-                                    rnd.Next(Convert.ToInt32(Max.Nivel)) + 1,           // nivel
-                                    rnd.Next(Convert.ToInt32(Max.Fuerza)) + 1,          // fuerza
-                                    rnd.Next(Convert.ToInt32(Max.Velocidad)) + 1,       // velocidad
-                                    rnd.Next(Convert.ToInt32(Max.Destreza)) + 1,        // destreza
-                                    rnd.Next(Convert.ToInt32(Max.Armadura)) + 1         // armadura
-                    );
+                    rnd.Next(Convert.ToInt32(Max.Nivel)) + 1,           // nivel
+                    rnd.Next(Convert.ToInt32(Max.Fuerza)) + 1,          // fuerza
+                    rnd.Next(Convert.ToInt32(Max.Velocidad)) + 1,       // velocidad
+                    rnd.Next(Convert.ToInt32(Max.Destreza)) + 1,        // destreza
+                    rnd.Next(Convert.ToInt32(Max.Armadura)) + 1         // armadura
+                );
 
                 personaje = new Personaje(datos, caracteristicas);
                 personajes.Add(personaje);
@@ -76,39 +93,39 @@ namespace Main
             return personajes;
         }
 
-        static void mostrarPersonajes(List<Personaje> lista)
+        static void MostrarPersonajes(List<Personaje> lista)
         {
             var cultura = CultureInfo.CreateSpecificCulture("es-ES");
 
             ConsoleTable tabla = new ConsoleTable(
-                                    "Nombre",
-                                    "Apodo",
-                                    "Tipo",
-                                    "Nacimiento",
-                                    "Edad",
-                                    "Salud",
-                                    "Nivel",
-                                    "Fuerza",
-                                    "Velocidad",
-                                    "Destreza",
-                                    "Armadura"
-                );
+                "Nombre",
+                "Apodo",
+                "Tipo",
+                "Nacimiento",
+                "Edad",
+                "Salud",
+                "Nivel",
+                "Fuerza",
+                "Velocidad",
+                "Destreza",
+                "Armadura"
+            );
 
             foreach (Personaje personaje in lista)
             {
                 tabla.AddRow(
-                                    personaje.Datos.Nombre,
-                                    personaje.Datos.Apodo,
-                                    personaje.Datos.Tipo,
-                                    personaje.Datos.Nacimiento.Date.ToString("d", cultura),
-                                    personaje.Datos.Edad(),
-                                    personaje.Datos.Salud,
-                                    personaje.Caracteristicas.Nivel,
-                                    personaje.Caracteristicas.Fuerza,
-                                    personaje.Caracteristicas.Velocidad,
-                                    personaje.Caracteristicas.Destreza,
-                                    personaje.Caracteristicas.Armadura
-                    );
+                    personaje.Datos.Nombre,
+                    personaje.Datos.Apodo,
+                    personaje.Datos.Tipo,
+                    personaje.Datos.Nacimiento.Date.ToString("d", cultura),
+                    personaje.Datos.Edad(),
+                    personaje.Datos.Salud,
+                    personaje.Caracteristicas.Nivel,
+                    personaje.Caracteristicas.Fuerza,
+                    personaje.Caracteristicas.Velocidad,
+                    personaje.Caracteristicas.Destreza,
+                    personaje.Caracteristicas.Armadura
+                );
             }
 
             tabla.Write();
@@ -118,40 +135,30 @@ namespace Main
         {
             Personaje[] resultado = new Personaje[2];
 
-            for (int i = 0; i < 3; i++)
+            while (personajeA.Datos.Salud > 0 && personajeB.Datos.Salud > 0)
             {
-                personajeA.Atacar(personajeB);
-                if (personajeB.Datos.Salud <= 0)
+                for (int i = 0; i < 3; i++)
                 {
-                    personajeA.Datos.Salud += +10;
-                    resultado[0] = personajeA;
-                    resultado[1] = personajeB;
-                    break;
-                }
+                    personajeA.Atacar(personajeB);
+                    if (personajeB.Datos.Salud <= 0)
+                    {
+                        personajeA.Datos.Salud += +10;
+                        resultado[0] = personajeA;
+                        resultado[1] = personajeB;
+                        break;
+                    }
 
-                personajeB.Atacar(personajeA);
-                if (personajeA.Datos.Salud <= 0)
-                {
-                    personajeB.Datos.Salud += +10;
-                    resultado[0] = personajeB;
-                    resultado[1] = personajeA;
-                    break;
+                    personajeB.Atacar(personajeA);
+                    if (personajeA.Datos.Salud <= 0)
+                    {
+                        personajeB.Datos.Salud += +10;
+                        resultado[0] = personajeB;
+                        resultado[1] = personajeA;
+                        break;
+                    }
                 }
             }
-
             return resultado;
-        }
-
-        static Personaje Batalla(List<Personaje> lista)
-        {
-            Personaje[] resultadoCombate = null;
-            while (lista.Count > 1)
-            {
-                resultadoCombate = Combate(lista[0], lista[1]);
-                lista.Remove(resultadoCombate[1]);
-            }
-
-            return lista[0];
         }
     }
 }
